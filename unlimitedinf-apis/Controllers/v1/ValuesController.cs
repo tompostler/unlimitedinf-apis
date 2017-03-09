339fb9b99012aca6cbbdbb99e05f710a5e91e691
Tom Postler <tom@postler.me>
2017-03-08 23:07:46 -0600

    Use TableStorage in prod

--- unlimitedinf-apis/Controllers/v1/ValuesController.cs
+++ unlimitedinf-apis/Controllers/v1/ValuesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Web.Http;
+﻿using Microsoft.Azure;
+using Microsoft.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +15,26 @@ namespace Unlimitedinf.Apis.Controllers.V1
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<DemoButt> Get()
         {
-            return new string[] { "value1", "value2" };
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("unlimitedinfapis_AzureStorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("demobutt");
+
+            // Check how many we've got
+            if (table.Exists())
+            {
+                TableQuery<DemoButt> query = new TableQuery<DemoButt>();
+                return table.ExecuteQuery(query);
+            }
+
+            return new List<DemoButt>();
         }
 
         // GET api/values/5
@@ -26,6 +46,41 @@ namespace Unlimitedinf.Apis.Controllers.V1
         // POST api/values
         public void Post([FromBody]string value)
         {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("unlimitedinfapis_AzureStorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("demobutt");
+
+            // Check how many we've got
+            if (table.Exists())
+            {
+                TableQuery<DemoButt> query = new TableQuery<DemoButt>();
+                if (table.ExecuteQuery(query).Count() > 10)
+                    table.DeleteIfExists();
+            }
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+
+            TableOperation insert = TableOperation.Insert(new DemoButt());
+            table.Execute(insert);
+        }
+
+        public class DemoButt : TableEntity
+        {
+            public DemoButt()
+            {
+                this.PartitionKey = Guid.NewGuid().ToString();
+                this.RowKey = Guid.NewGuid().ToString();
+                this.Insertion = DateTime.Now;
+            }
+
+            public DateTime Insertion { get; set; }
         }
 
         // PUT api/values/5
