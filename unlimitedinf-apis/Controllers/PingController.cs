4b661b64668bc265c1f613a30f695357ee6f6872
Tom Postler <Tom.Postler@microsoft.com>
2017-03-13 17:33:45 -0500

    Add ping controller

---
+++ unlimitedinf-apis/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace Unlimitedinf.Apis.Controllers
+{
+    [RoutePrefix("ping")]
+    public class PingController : ApiController
+    {
+        [Route, HttpGet, HttpHead, HttpOptions]
+        public IHttpActionResult Ping()
+        {
+            return Content<object>(HttpStatusCode.NoContent, null);
+        }
+    }
+}
