051c55f39ec7fb20e0e147e0253a04c59cac7158
Tom Postler <Tom.Postler@microsoft.com>
2017-04-20 16:47:33 -0500

    Started basic ApiClient work

---
+++ Unlimitedinf.Apis/ApiClientAuth.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Unlimitedinf.Apis.Contracts.Auth;
+
+namespace Unlimitedinf.Apis
+{
+    /// <summary>
+    /// A helper class to handle the authentication CRUD necessary for the ApiClient.
+    /// </summary>
+    public static class ApiClientAuth
+    {
+        private const string BaseUrl = "https://unlimitedinf-apis.azurewebsites.net";
+
+        private const string AuthBase = BaseUrl + "/auth";
+        private const string AuthAccount = AuthBase + "/accounts";
+        private const string AuthToken = AuthBase + "/tokens";
+
+        /// <summary>
+        /// Method to create an account.
+        /// </summary>
+        /// <param name="account">The account information.</param>
+        public static async Task<Account> CreateAccount(Account account)
+        {
+            HttpResponseMessage response;
+            using (var httpClient = new HttpClient())
+            {
+                response = await httpClient.PostAsync(AuthAccount, new StringContent(JsonConvert.SerializeObject(account)));
+            }
+
+            //TODO: Check for non-success state
+
+            return JsonConvert.DeserializeObject<Account>(await response.Content.ReadAsStringAsync());
+        }
+
+        /// <summary>
+        /// Generate a token to be used with the constructor of this class.
+        /// </summary>
+        /// <param name="creation">The parameters for creating a token.</param>
+        public static async Task<Token> CreateToken(TokenCreate creation)
+        {
+            HttpResponseMessage response;
+            using (var httpClient = new HttpClient())
+            {
+                response = await httpClient.PostAsync(AuthToken, new StringContent(JsonConvert.SerializeObject(creation)));
+            }
+
+            //TODO: Check for non-success state
+
+            return JsonConvert.DeserializeObject<Token>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
