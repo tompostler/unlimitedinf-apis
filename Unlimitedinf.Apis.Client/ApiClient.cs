e93bb2feabb641e8080923c13879e7c95b625100
Tom Postler <tompostler@outlook.com>
2017-05-10 11:00:22 -0500

    Start of ApiClient

---
+++ Unlimitedinf.Apis.Client/ApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CA = Unlimitedinf.Apis.Contracts.Auth;
+
+namespace Unlimitedinf.Apis.Client
+{
+    /// <summary>
+    /// The main client for the APIs.
+    /// </summary>
+    public sealed partial class ApiClient
+    {
+        private string Token { get; set; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public ApiClient(string token)
+        {
+            if (CA.Token.IsTokenExpired(token))
+                throw new ArgumentException("Token is expired.", nameof(token));
+            this.Token = token;
+
+            this.Version = new Versioning(this.Token);
+        }
+
+        public Versioning Version { get; }
+    }
+}
---
+++ Unlimitedinf.Apis.Client/ApiClient_Versioning.cs
@@ -0,0 +1,17 @@
+﻿using CA = Unlimitedinf.Apis.Contracts.Auth;
+
+namespace Unlimitedinf.Apis.Client
+{
+    public sealed partial class ApiClient
+    {
+        public sealed class Versioning
+        {
+            private string Token { get; set; }
+
+            internal Versioning(string token)
+            {
+                this.Token = token;
+            }
+        }
+    }
+}
