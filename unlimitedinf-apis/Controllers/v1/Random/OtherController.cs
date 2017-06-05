6ca27034aed76f89a280811955f838892ce2e9f2
Tom Postler <Tom.Postler@microsoft.com>
2017-06-05 17:55:38 -0500

    random guid

---
+++ unlimitedinf-apis/Controllers/v1/Random/OtherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Web.Http;
+using System;
+using System.Web.Http;
+
+namespace Unlimitedinf.Apis.Controllers.v1.Random
+{
+    [ApiVersion("1.0")]
+    [RoutePrefix("random")]
+    public class OtherController : ApiController
+    {
+        [Route("guid"), HttpGet]
+        public IHttpActionResult GuidMethod()
+        {
+            return Ok(Guid.NewGuid().ToString("D"));
+        }
+    }
+}
