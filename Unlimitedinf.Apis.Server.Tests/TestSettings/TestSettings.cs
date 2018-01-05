a434083e4908cfad1b4507273afdf71ffcca7a6d
Tom Postler <tom@postler.me>
2018-01-04 22:40:52 -0600

    Start attempt at tests

---
+++ Unlimitedinf.Apis.Server.Tests/TestSettings/TestSettings.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Unlimitedinf.Apis.Server.Tests
+{
+    public sealed class TestSettings
+    {
+        public static TestSettings I { get; }
+
+        static TestSettings()
+        {
+            I = JsonConvert.DeserializeObject<TestSettings>(
+                File.ReadAllText(
+                    Path.Combine(
+                        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                        "TestSettings",
+                        "testsettings.json")));
+        }
+
+        public Uri BaseUri { get; set; }
+    }
+}
---
+++ Unlimitedinf.Apis.Server.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Unlimitedinf.Apis.Server.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Debugger.Break();
+        }
+    }
+}
