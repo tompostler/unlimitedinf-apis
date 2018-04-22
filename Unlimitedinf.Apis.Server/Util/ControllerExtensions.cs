d3e6b385c020ebcfe47dff9b9cd7f2b106229c7f
Tom Postler <tom@postler.me>
2018-04-22 17:07:02 -0500

    first attempt at pretty-print json returns

--- Unlimitedinf.Apis.Server/Util/ControllerExtensions.cs
+++ Unlimitedinf.Apis.Server/Util/ControllerExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Net;
 
 namespace Unlimitedinf.Apis.Server.Util
@@ -19,5 +21,23 @@ namespace Unlimitedinf.Apis.Server.Util
         {
             return !cont.User.Identity.Name.Equals(username, StringComparison.OrdinalIgnoreCase);
         }
+
+        //public static OkObjectResult Ok(this Controller cont, object result)
+        //{
+        //    bool wantsHtml = false;
+
+        //    if (cont.Request.Headers.ContainsKey("Accept"))
+        //    {
+        //        wantsHtml = cont.Request.Headers["Accept"].Contains("text/html");
+        //    }
+
+        //    if (!(result is string) && wantsHtml)
+        //        // If the content is not a string and the accept types ask for text/html
+        //        // (aka the browser is hitting this url)
+        //        //  then pretty-print the json
+        //        return cont.Ok(JsonConvert.SerializeObject(result, Formatting.Indented));
+
+        //    return cont.Ok(result);
+        //}
     }
 }
